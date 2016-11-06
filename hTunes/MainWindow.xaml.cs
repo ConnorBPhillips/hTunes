2949859fb52b75d718773812cef1af92b88a1e29
destradaHU <destrada@harding.edu>
2016-11-06 16:20:22 -0600

    Changes to the UI and listbox

--- hTunes/MainWindow.xaml.cs
+++ hTunes/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace hTunes
     {
         private DataSet musicDataSet = new DataSet();
         private MusicLib musicLib = new MusicLib();
+        private Point startPoint;
 
         public MainWindow()
         {
@@ -173,6 +174,53 @@ namespace hTunes
         private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
         {
             musicLib.Save();
+        }
+
+        private void dataGrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Get the current mouse position
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startPoint - mousePos;
+
+            // Start the drag-drop if mouse has moved far enough
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                // Initiate dragging the text from the datagrid
+                DragDrop.DoDragDrop(dataGrid, ExtractSelectedSongID(), DragDropEffects.Copy);
+            }
+        }
+
+        private void dataGrid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Store the mouse position
+            startPoint = e.GetPosition(null);
+        }
+
+        private int ExtractSelectedSongID()
+        {
+            int songId = 0;
+            DataRowView rowView = dataGrid.SelectedItem as DataRowView;
+            if (rowView != null)
+            {
+                // Extract the song ID from the selected song
+                songId = Convert.ToInt32(rowView.Row.ItemArray[0]);
+            }
+            return songId;
+        }
+
+        private void listBox_Drop(object sender, DragEventArgs e)
+        {
+            // If the DataObject contains string data, extract it
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+                int songId = Convert.ToInt32(dataString);
+
+                musicLib.AddSongToPlaylist(songId, listBox.SelectedValue.ToString());
+            }
+
         }
     }
 
